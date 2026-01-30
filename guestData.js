0aa65e59e21bd4c20007bb53aca0ba88431f340d
ChanulaHerath <chanula.herath@sitecore.com>
2026-01-30 15:14:22 +0530

    adding more guests and changing site title

--- guestData.js
+++ guestData.js
@@ -5,14 +5,6 @@ const guestDatabase = {
     // Format: 'name': { guestCount: number, invitedTo: 'both'|'church'|'reception' }
 
     // Example guests invited to both
-    'Luqman Deane': {
-        guestCount: 4,
-        invitedTo: 'both'
-    },
-    'Dinith Ranmuthugala': {
-        guestCount: 1,
-        invitedTo: 'reception'
-    },
     'Chanula Herath': {
         guestCount: 1,
         invitedTo: 'reception'
@@ -22,8 +14,239 @@ const guestDatabase = {
         invitedTo: 'both'
     },
 
-    // Add more guests here...
-    // Copy the format above for each guest
+    'Luqman Deane': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Dinith Ranmuthugala': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Theeksha Gunasinghe': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Dileesha Hettiarachchi': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Raizul Deen': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Rihan Kottage': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Indoopa Samarasinghe': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Rochelle Benjamin': {
+        guestCount: 2,
+        invitedTo: 'both'
+    },
+    'Raveesha Munasinghe': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Amaan Ajeer': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Binuri Gunawardena': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Maleesha Withanage': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Jerome Andrew': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Pubudu Wickramasinghe': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Amavi Kawya': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Thiwanka Katangodage': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Nadeesha Hansani': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Menukshi Gunasekera': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Devki Gamage': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Irushi Gunathilake': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Britney Martil': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Dilhari Fernando': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Manumi Ranagala': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Hirushi Bataduwaarachchi': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Imasha Rodrigo': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Manuli Kalutharaarachchi': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Amandi Perera': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Randithi Jayawardena': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Nelini Fernando': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Chethavi Ariyasinghe': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Dileesha Perera': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Heshani Gunawardena': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Piyum Karunasena': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Nushan Kodikara': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Nethmi Atapattu': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Olinda Amarasekera': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Aruna Dissanayake': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Reshan Warnakulasuriya': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Harini Jayasinghe': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Hashila Hennayake': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Nimantha Jayawardene': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Sandaruwan Kumarasinghe': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Pathum Wijethunge': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Nadun Atugoda': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Tharinda Kandanaarachchi': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Deepal De Silva': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Chandima Wijekoon': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Pramod Dissanayaka': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Isuru Samaranayake': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Ishan Uditha': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Waruna Kodithuwakku': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Shalintha Thaviru': {
+        guestCount: 1,
+        invitedTo: 'reception'
+    },
+    'Chamath Wikramasinghe': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Shanya Karunaratne': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Ishan Gunawardene': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Senali Kalpage': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Chindumi Karunaarachchi': {
+        guestCount: 1,
+        invitedTo: 'both'
+    },
+    'Vihara Weerasooriya': {
+        guestCount: 1,
+        invitedTo: 'both'
+    }
+
 };
 
 // Helper function to get guest data by name
